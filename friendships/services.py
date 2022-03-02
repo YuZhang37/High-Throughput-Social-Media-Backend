28155705a64deaecf1b7fff5bed050cf77057116
Yu Zhang <mzhangyjoy@gmail.com>
2022-03-02 12:54:17 -0600

    friendship page paginations

--- friendships/services.py
+++ friendships/services.py
@@ -13,3 +13,12 @@ class FriendshipService:
             for friendship in friendships
         ]
         return followers
+
+    @classmethod
+    def has_followed(cls, from_user, to_user):
+        # is_anonymous is property in User and AnonymousUser
+        if from_user.is_anonymous or to_user.is_anonymous:
+            return False
+        return Friendship.objects\
+            .filter(from_user=from_user, to_user=to_user)\
+            .exists()
