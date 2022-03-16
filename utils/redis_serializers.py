888ee308d4d80c6688b984f02f3e0a411ffa11fc
Yu Zhang <mzhangyjoy@gmail.com>
2022-03-15 20:34:58 -0500

    install redis and serializers for redis

---
+++ utils/redis_serializers.py
@@ -0,0 +1,16 @@
+from django.core import serializers
+from utils.json_encoder import JSONEncoder
+
+
+class RedisModelSerializer:
+
+    @classmethod
+    def serialize(cls, obj):
+        serialized_data = serializers.serialize('json', [obj], cls=JSONEncoder)
+        return serialized_data
+
+    @classmethod
+    def deserialize(cls, data):
+        objs = serializers.deserialize('json', data)
+        model_obj = list(objs)[0].object
+        return model_obj
