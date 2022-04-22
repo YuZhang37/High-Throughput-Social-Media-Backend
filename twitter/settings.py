f02d461d36adb29f2ae0ea62bf50bc6816661f0e
Yu Zhang <mzhangyjoy@gmail.com>
2022-04-22 16:54:17 -0500

    fix the problem with gunicron having no ip

--- twitter/settings.py
+++ twitter/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "utils.middlewares.XForwardedForMiddleware",
 ]
 
 ROOT_URLCONF = 'twitter.urls'
